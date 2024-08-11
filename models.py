c932bc507d32e59afebc73b1271ffaad0fac165d
Damian Trafialek <damian.trafialek@gmail.com>
2024-08-11 21:28:05 +0200

    add: get_params functions

--- models.py
+++ models.py
@@ -23,28 +23,60 @@ class ModelManager:
 
     def get_model_wrappers(self) -> List[ModelWrapper]:
         # --- TODO ---
-        # Add more taks types and specify the params optimization
+        # Add more taks types and models
         if self.task == "classification":
             return [
                 ModelWrapper(
                     model=RidgeClassifier(),
                     allow_strings=False,
-                    get_params=lambda x: {},
+                    get_params=lambda trial: {
+                        "alpha": trial.suggest_float(
+                            "ridge_alpha", 1e-3, 100, log=True
+                        ),
+                        "tol": trial.suggest_float("ridge_tol", 1e-5, 1e-1, log=True),
+                    },
                 ),
                 ModelWrapper(
                     model=RandomForestClassifier(),
                     allow_strings=True,
-                    get_params=lambda x: {},
+                    get_params=lambda trial: {
+                        "n_estimators": trial.suggest_int("rf_n_estimators", 50, 500),
+                        "max_depth": trial.suggest_int("rf_max_depth", 3, 20),
+                        "min_samples_split": trial.suggest_int(
+                            "rf_min_samples_split", 2, 20
+                        ),
+                        "min_samples_leaf": trial.suggest_int(
+                            "rf_min_samples_leaf", 1, 20
+                        ),
+                        "max_features": trial.suggest_categorical(
+                            "rf_max_features", ["auto", "sqrt", "log2"]
+                        ),
+                    },
                 ),
                 ModelWrapper(
                     model=KNeighborsClassifier(),
                     allow_strings=True,
-                    get_params=lambda x: {},
+                    get_params=lambda trial: {
+                        "n_neighbors": trial.suggest_int("knn_n_neighbors", 3, 50),
+                        "weights": trial.suggest_categorical(
+                            "knn_weights", ["uniform", "distance"]
+                        ),
+                        "p": trial.suggest_int("knn_p", 1, 5),
+                    },
                 ),
                 ModelWrapper(
                     model=SVC(),
                     allow_strings=True,
-                    get_params=lambda x: {},
+                    get_params=lambda trial: {
+                        "C": trial.suggest_float("svc_C", 1e-3, 10, log=True),
+                        "kernel": trial.suggest_categorical(
+                            "svc_kernel", ["linear", "poly", "rbf", "sigmoid"]
+                        ),
+                        "gamma": trial.suggest_categorical(
+                            "svc_gamma", ["scale", "auto"]
+                        ),
+                        "tol": trial.suggest_float("svc_tol", 1e-5, 1e-1, log=True),
+                    },
                 ),
             ]
         else:
