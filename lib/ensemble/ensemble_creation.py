d46a45f71b61af9ae2ce04b3e0ee2857d53cbcea
Damian Trafialek <damian.trafialek@gmail.com>
2024-10-09 07:35:50 +0200

    fix: ensemble parallelism

--- lib/ensemble/ensemble_creation.py
+++ lib/ensemble/ensemble_creation.py
@@ -1,8 +1,10 @@
 from pathlib import Path
 import pickle
 from typing import Callable, List, Tuple, TypedDict, cast
+import multiprocessing as mp
 
 import pandas as pd
+from sklearn.model_selection import KFold
 
 from lib.logger import setup_logger
 from lib.models.EnsembleModel import EnsembleModel
@@ -65,8 +67,132 @@ def create_ensemble_model(
     engineered_data = function_dto["engineer_features_func"](train).set_index("id")
 
     logger.info("Running optimization")
-    final_ensemble_model, ensemble_result_df = ensemble_model.optimize(
-        X=engineered_data, y=engineered_data["class"], n_cv=n_cv, processes=processes
+    final_ensemble_model, ensemble_result_df = optimize_ensemble(
+        ensemble_model=ensemble_model,
+        X=engineered_data,
+        y=engineered_data["class"],
+        n_cv=n_cv,
+        processes=processes,
     )
 
     return final_ensemble_model, ensemble_result_df
+
+
+def evaluate_combination(
+    j: int,
+    y_test: pd.Series,
+    combination_names: List[str],
+    scores: List[float],
+    predictions: List[pd.Series],
+) -> Tuple[str, float]:
+
+    logger.info(f"Evaluating combination {j}")
+    combination_names_string = "-".join(combination_names)
+
+    temp_ensemble = EnsembleModel(
+        models=[],
+        combination_feature_lists=[],
+        combination_names=combination_names,
+        scores=scores,
+        processing_pipelines=[],
+        predictions=predictions,
+    )
+
+    y_pred = temp_ensemble._combine_classification_predictions()
+
+    accuracy = (y_pred == y_test).sum() / len(y_test)
+
+    logger.info(f"Finished evaluating combination {j}")
+
+    return combination_names_string, accuracy
+
+
+def optimize_ensemble(
+    ensemble_model: EnsembleModel,
+    X: pd.DataFrame,
+    y: pd.Series,
+    n_cv: int,
+    processes: int,
+) -> Tuple[EnsembleModel, pd.DataFrame]:
+    logger.info("Optimizing ensemble model | " + "Optimizing with bitmap")
+
+    processes = mp.cpu_count() if processes == -1 else processes
+    kfold = KFold(n_splits=n_cv)
+    results_list: List[Tuple[str, int, float]] = []
+
+    for cnt, (train_index, test_index) in enumerate(kfold.split(X)):
+        X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+        y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+        logger.info(f"Optimizing fold {cnt + 1}")
+
+        ensemble_model.fit(X_train, y_train)
+        if ensemble_model.processing_pipelines is None:
+            raise ValueError("The ensemble model has not been fitted yet")
+
+        ensemble_model.predict(X_test)
+        if ensemble_model.predictions is None:
+            raise ValueError("The ensemble model has not been predicted yet")
+
+        logger.info(f"Evaluating combinations with {processes} processes")
+
+        with mp.Pool(processes=processes) as pool:
+
+            tasks = [
+                pool.apply_async(
+                    evaluate_combination,
+                    (
+                        j,
+                        y_test,
+                        [
+                            ensemble_model.combination_names[k]
+                            for k in range(len(ensemble_model.models))
+                            if j & (1 << k)
+                        ],
+                        [
+                            ensemble_model.scores[k]
+                            for k in range(len(ensemble_model.models))
+                            if j & (1 << k)
+                        ],
+                        [
+                            ensemble_model.predictions[k].copy()
+                            for k in range(len(ensemble_model.models))
+                            if j & (1 << k)
+                        ],
+                    ),
+                )
+                for j in range(1, 20)
+            ]
+
+            for h, task in enumerate(tasks):
+                temp_combination_names_string, temp_accuracy = task.get()
+                results_list.append((temp_combination_names_string, h, temp_accuracy))
+
+    results_df = pd.DataFrame(
+        results_list,
+        columns=["combination", "fold", "score"],
+    )
+    results_df = results_df.groupby("combination")["score"].mean()
+    logger.info(f"Optimization results: {results_df}")
+    best_combination = str(results_df.idxmax())
+
+    best_combination_names = best_combination.split("-")
+
+    best_ensemble = EnsembleModel(
+        models=[
+            ensemble_model.models[ensemble_model.combination_names.index(name)]
+            for name in best_combination_names
+        ],
+        combination_feature_lists=[
+            ensemble_model.combination_feature_lists[
+                ensemble_model.combination_names.index(name)
+            ]
+            for name in best_combination_names
+        ],
+        combination_names=best_combination_names,
+        scores=[
+            ensemble_model.scores[ensemble_model.combination_names.index(name)]
+            for name in best_combination_names
+        ],
+    )
+
+    return best_ensemble, results_df.to_frame()
--- lib/models/EnsembleModel.py
+++ lib/models/EnsembleModel.py
@@ -1,11 +1,9 @@
-from typing import List, Optional, Tuple
+from typing import List, Optional
 from dataclasses import dataclass
-import multiprocessing as mp
 
 import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator
-from sklearn.model_selection import KFold
 from sklearn.pipeline import Pipeline
 
 from lib.logger import setup_logger
@@ -97,118 +95,3 @@ class EnsembleModel(BaseEstimator):
 
         final_vote = votes.idxmax(axis=1)
         return final_vote
-
-    def _evaluate_combination(self, j: int, y_test: pd.Series) -> Tuple[str, float]:
-
-        if self.processing_pipelines is None:
-            raise ValueError("The ensemble model has not been fitted yet")
-
-        if self.predictions is None:
-            raise ValueError("The ensemble model has not been predicted yet")
-
-        temp_combination_names = [
-            self.combination_names[k] for k in range(len(self.models)) if j & (1 << k)
-        ]
-        temp_combination_names_string = "-".join(temp_combination_names)
-
-        temp_ensemble = EnsembleModel(
-            models=[self.models[k] for k in range(len(self.models)) if j & (1 << k)],
-            combination_feature_lists=[
-                self.combination_feature_lists[k]
-                for k in range(len(self.models))
-                if j & (1 << k)
-            ],
-            combination_names=temp_combination_names,
-            scores=[self.scores[k] for k in range(len(self.models)) if j & (1 << k)],
-            processing_pipelines=[
-                self.processing_pipelines[k]
-                for k in range(len(self.models))
-                if j & (1 << k)
-            ],
-            predictions=[
-                self.predictions[k] for k in range(len(self.models)) if j & (1 << k)
-            ],
-        )
-
-        y_pred = temp_ensemble._combine_classification_predictions()
-
-        temp_accuracy = (y_pred == y_test).sum() / len(y_test)
-
-        return temp_combination_names_string, temp_accuracy
-
-    def optimize(
-        self,
-        n_cv: int,
-        X: pd.DataFrame,
-        y: pd.Series,
-        processes: int = -1,
-    ) -> Tuple["EnsembleModel", pd.DataFrame]:
-        logger.info("Optimizing ensemble model | " + "Optimizing with bitmap")
-
-        processes = mp.cpu_count() if processes == -1 else processes
-        kfold = KFold(n_splits=n_cv)
-
-        results_list: List[Tuple[str, int, float]] = []
-
-        for i, (train_index, test_index) in enumerate(kfold.split(X)):
-            X_train, X_test = X.iloc[train_index], X.iloc[test_index]
-            y_train, y_test = y.iloc[train_index], y.iloc[test_index]
-            logger.info(f"Optimizing fold {i + 1}")
-
-            self.fit(X_train, y_train)
-            if self.processing_pipelines is None:
-                raise ValueError("The ensemble model has not been fitted yet")
-
-            self.predict(X_test)
-            if self.predictions is None:
-                raise ValueError("The ensemble model has not been predicted yet")
-
-            with mp.Pool(processes) as pool:
-                tasks = [
-                    pool.apply_async(
-                        self._evaluate_combination,
-                        args=(j, y_test),
-                    )
-                    for j in range(1, 2 ** len(self.models))
-                ]
-
-                for k, task in enumerate(tasks, start=1):
-                    log_interval = max(1, (2 ** len(self.models)) // 10)
-                    if k % log_interval == 0:
-                        logger.info(f"Checking combination {k}/{2 ** len(self.models)}")
-
-                    temp_combination_names_string, temp_accuracy = task.get()
-
-                    results_list.append(
-                        (temp_combination_names_string, i, temp_accuracy)
-                    )
-
-        results_df = pd.DataFrame(
-            results_list,
-            columns=["combination", "fold", "score"],
-        )
-
-        results_df = results_df.groupby("combination")["score"].mean()
-        logger.info(f"Optimization results: {results_df}")
-
-        best_combination = str(results_df.idxmax())
-
-        best_combination_names = best_combination.split("-")
-
-        best_ensemble = EnsembleModel(
-            models=[
-                self.models[self.combination_names.index(name)]
-                for name in best_combination_names
-            ],
-            combination_feature_lists=[
-                self.combination_feature_lists[self.combination_names.index(name)]
-                for name in best_combination_names
-            ],
-            combination_names=best_combination_names,
-            scores=[
-                self.scores[self.combination_names.index(name)]
-                for name in best_combination_names
-            ],
-        )
-
-        return best_ensemble, results_df.to_frame()
